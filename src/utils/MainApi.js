7f5b9b70d583899837f9fb8dc34cc77390b7d0fa
Alex Titov <cloud9.alex.tch@gmail.com>
2024-02-03 20:22:34 +0300

    feat: finished MainApi

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -3,6 +3,7 @@ class MainApi {
         this._baseUrl = options.baseUrl;
         this._headers = options.headers;
     }
+
     _getResponseData(res) {
         return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
     }
@@ -30,6 +31,56 @@ class MainApi {
                 name: name,
                 email: email
             })
+        }).then(res => {
+            return this._getResponseData(res);
+        });
+    }
+
+    getSavedMovies() {
+        return fetch(`${this._baseUrl}/movies`, {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('jwt')}`,
+                ...this._headers
+            }
+        }).then(res => {
+            return this._getResponseData(res);
+        });
+    }
+
+    createNewMovie(movie) {
+        return fetch(`${this._baseUrl}/movies`, {
+            method: 'POST',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('jwt')}`,
+                ...this._headers
+            },
+            body: JSON.stringify({
+                country: movie.country,
+                director: movie.director,
+                duration: movie.duration,
+                year: movie.year,
+                description: movie.description,
+                image: movie.image,
+                trailerLink: movie.trailerLink,
+                thumbnail: movie.thumbnail,
+                owner: movie.owner,
+                movieId: movie.movieId,
+                nameRU: movie.nameRU,
+                nameEN: movie.nameEN
+            })
+        }).then(res => {
+            return this._getResponseData(res);
+        });
+    }
+
+    deleteMovie(movieId) {
+        return fetch(`${this._baseUrl}/cards/${movieId}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('jwt')}`,
+                ...this._headers
+            }
         }).then(res => {
             return this._getResponseData(res);
         });
